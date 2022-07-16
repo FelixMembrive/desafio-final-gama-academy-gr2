8350d2ef11981cbd9a19e29ab5cc7b1f7b5c0207
FelixMembrive <felix.membrive@gmail.com>
2022-07-15 21:55:01 -0300

    Instalando o Bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import Button from 'react-bootstrap/Button';
+import ModalBS from './componentes/ModalBS/ModalBS';
+import AlertBS from './componentes/TesteBS/AlertBS';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <>
+      <h1>DESAFIO FINAL</h1>
+      <Button>Botão BS TESTE</Button>
+      <AlertBS />
+      <ModalBS />
+    </>
   )
 }
-
-export default App
---
+++ src/componentes/TesteBS/AlertBS.tsx
@@ -0,0 +1,22 @@
+import { Alert } from "react-bootstrap";
+
+export default function AlertBS() {
+    return (
+        <>
+            {[
+                'primary',
+                'secondary',
+                'success',
+                'danger',
+                'warning',
+                'info',
+                'light',
+                'dark',
+            ].map((variant) => (
+                <Alert key={variant} variant={variant}>
+                    This is a {variant} alert—check it out!
+                </Alert>
+            ))}
+        </>
+    );
+}
