0ca89202f262e55a773aae7872030c6910808067
Marcelo Barbosa <celo.filho1@hotmail.com>
2022-07-18 20:10:32 -0300

    Teste-Marcelo

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
-import React from 'react';
-import RoutesComponent from "./Routes";
+import React from 'react'
+import RoutesComponent from './Routes'
 
 export default function App() {
-  return (
-    <RoutesComponent />
-  )
+  return <RoutesComponent />
 }
+//teste
