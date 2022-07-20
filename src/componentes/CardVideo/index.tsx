883f315a76fa5198e9ac03bfd42c7d65d5bcae65
Beatriz Lisboa Sanchez <bia.lsanchez@gmail.com>
2022-07-20 13:51:04 -0300

    styled basic home depo look

--- src/componentes/CardVideo/index.tsx
+++ src/componentes/CardVideo/index.tsx
@@ -1,16 +1,25 @@
 import { Col, Row } from "react-bootstrap";
 import Card from "react-bootstrap/Card";
+import './style.css'
 
-export default function CardVideo() {
+interface ICardVideoProps {
+    videoTitle?: string;
+    src: string;
+    cardTitle: string;
+    cardSubtitle: string;
+    cardText: string;
+}
+
+export default function CardVideo(props: ICardVideoProps) {
   return (
-      <Col>
-        <Card style={{ width: "18rem" }}>
-          <Card.Img variant="top" src="https://picsum.photos/200/200" />
+      <Col className="card-col" style={{ width: "23rem"}}>
+        <Card style={{ width: "23rem"}} className="card">
+        <iframe className="card-img-top img-fluid" src={props.src} title={props.videoTitle? props.videoTitle: "video depoimento"} frameBorder={0} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
           <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
+            <Card.Title className="fs-5">{props.cardTitle}</Card.Title>
+            <Card.Subtitle className="">{props.cardSubtitle}</Card.Subtitle>
+            <Card.Text className="fs-6">
+              {props.cardText}
             </Card.Text>
           </Card.Body>
         </Card>
--- src/componentes/Depoimentos/index.tsx
+++ src/componentes/Depoimentos/index.tsx
@@ -2,26 +2,70 @@ import CardVideo from "../CardVideo";
 import ScrollContainer from 'react-indiana-drag-scroll'
 import { Row } from "react-bootstrap";
 
+import './style.css'
+
 export default function Depoimentos() {
+    const dados = [
+        {
+            src: "https://www.youtube.com/embed/NpEaa2P7qZI",
+            nome: "Mariana Benício Porto",
+            profissao: "Product Designer",
+            empresa: "Nome da Empresa",
+            texto: "Some quick example text to build on the card title and make up the bulk of the card's content."
+        },
+        {
+            src: "https://www.youtube.com/embed/NpEaa2P7qZI",
+            nome: "Cauane Maria Nogueira",
+            profissao: "Product Designer",
+            empresa: "Nome da Empresa",
+            texto: "Some quick example text to build on the card title and make up the bulk of the card's content."
+        },
+        {
+            src: "https://www.youtube.com/embed/NpEaa2P7qZI",
+            nome: "Liz Maitê Aparecida dos Santos",
+            profissao: "Product Designer",
+            empresa: "Nome da Empresa",
+            texto: "Some quick example text to build on the card title and make up the bulk of the card's content."
+        },
+        {
+            src: "https://www.youtube.com/embed/NpEaa2P7qZI",
+            nome: "Silvana Helena da Silva",
+            profissao: "Product Designer",
+            empresa: "Nome da Empresa",
+            texto: "Some quick example text to build on the card title and make up the bulk of the card's content."
+        },
+        {
+            src: "https://www.youtube.com/embed/NpEaa2P7qZI",
+            nome: "Vitoria Paula Santos Jesus",
+            profissao: "Product Designer",
+            empresa: "Nome da Empresa",
+            texto: "Some quick example text to build on the card title and make up the bulk of the card's content."
+        },
+        {
+            src: "https://www.youtube.com/embed/NpEaa2P7qZI",
+            nome: "Hadassa Isabella Cavalcanti",
+            profissao: "Product Designer",
+            empresa: "Nome da Empresa",
+            texto: "Some quick example text to build on the card title and make up the bulk of the card's content."
+        },
+        {
+            src: "https://www.youtube.com/embed/NpEaa2P7qZI",
+            nome: "Lara Maria Aragão",
+            profissao: "Product Designer",
+            empresa: "Nome da Empresa",
+            texto: "Some quick example text to build on the card title and make up the bulk of the card's content."
+        },
+    ]
   return (
-    <div>
+    <div className="depo-section">
         <h2>O que as pessoas dizem</h2>
-        <ScrollContainer className="scroll-container">
-        <Row className="flex-nowrap">
-        <CardVideo />
-        <CardVideo />
-        <CardVideo />
-        <CardVideo />
-        <CardVideo />
-        <CardVideo />
-        <CardVideo />
-        <CardVideo />
-        <CardVideo />
-        <CardVideo />
-        <CardVideo />
-        <CardVideo />
-        <CardVideo />
-        <CardVideo />
+        <ScrollContainer className="scroll-container d-grid">
+        <Row className="flex-nowrap row">
+        {dados.map((cardInfo) => {
+            return (
+                <CardVideo videoTitle="video placeholder" src={cardInfo.src} cardTitle={cardInfo.nome} cardSubtitle={`${cardInfo.profissao}, ${cardInfo.empresa}`} cardText={cardInfo.texto}/>
+            )
+        })}
         </Row>
       </ScrollContainer>
     </div>
