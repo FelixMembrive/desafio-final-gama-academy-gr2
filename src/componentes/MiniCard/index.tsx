b4ef04e0e93ce9325418cfed43ce7484e81a9000
“Pedro <pedro_brandao2@hotmail.com>
2022-07-20 16:09:33 -0300

    changed minicard class names to fix bug; Changed conteudos exclusivos section style from styled components to raw css

--- src/componentes/MiniCard/index.tsx
+++ src/componentes/MiniCard/index.tsx
@@ -1,5 +1,4 @@
-import { Col, Row } from "react-bootstrap";
-import Card from "react-bootstrap/Card";
+import { Col } from "react-bootstrap";
 import './styles.css'
 
 interface IMiniCard {
@@ -11,12 +10,12 @@ interface IMiniCard {
 export default function MiniCard(props: IMiniCard) {
   return (
     <Col>
-      <div className="card">
-        <img src={props.imgsrc}/>
-      <div className="card-content">
-        <h5>{props.title}</h5>
-        <p>{props.text}</p>
-      </div>
+      <div className="card-conteudo-exclusivo">
+          <img src={props.imgsrc}/>
+        <div>
+          <h5>{props.title}</h5>
+          <p>{props.text}</p>
+        </div>
       </div>
   </Col>
   );
