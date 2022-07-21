30140189d84160b6713b1997c4fb5430f4b0445e
FelixMembrive <felix.membrive@gmail.com>
2022-07-20 23:41:10 -0300

    Criando os componentes do Footer

--- src/componentes/Footer/index.tsx
+++ src/componentes/Footer/index.tsx
@@ -1,3 +1,5 @@
+import { Facebook, Instagram, Linkedin, Twitter, Whatsapp, Youtube } from "../../assets/icons";
+import LinkIcon from "../LinkIcons";
 import LinkText from "../LinkText";
 
 export default function Footer() {
@@ -9,24 +11,36 @@ export default function Footer() {
                     <LinkText to="cursos" text="Cursos " />
                     <LinkText to="mentoria" text="Mentoria " />
                     <LinkText to="blog" text="Blog " />
-                    <LinkText to="empresas parceiras" text="Empresas parceiras " />
+                    <LinkText to="empresas" text="Empresas parceiras " />
                     <LinkText to="sobre" text="Sobre nós " />
                     <LinkText to="faq" text="FAQ" />
                     <LinkText to="contato" text="Contato " />
                     <LinkText to="ajuda" text="Ajuda" />
                 </div>
                 <div className="bg-info col">
-                    <LinkText to="processos" text="Processos seletivos " />
-                    <LinkText to="cursos" text="Cursos" />
-                    <LinkText to="mentoria" text="Mentoria " />
-                    <LinkText to="blog" text="Blog" />
-                    <LinkText to="empresas parceiras" text="Empresas parceiras " />
-                    <LinkText to="sobre" text="Sobre nós " />
-                    <LinkText to="faq" text="FAQ" />
-                    <LinkText to="contato" text="Contato " />
-                    <LinkText to="ajuda" text="Ajuda " />
+                    <LinkIcon to="/facebook">
+                        <Facebook />
+                    </LinkIcon>
+                    <LinkIcon to="/instagram">
+                        <Instagram />
+                    </LinkIcon>
+                    <LinkIcon to="/linkedin">
+                        <Linkedin />
+                    </LinkIcon>
+                    <Twitter />
+                    <LinkIcon to="/whatsapp">
+                        <Whatsapp />
+                    </LinkIcon>
+                    <LinkIcon to="/youtube">
+                        <Youtube />
+                    </LinkIcon>
+                    <LinkIcon to="/twitter">
+                        <Twitter />
+                    </LinkIcon>
                 </div>
+
             </div>
         </div>
+
     );
 }
