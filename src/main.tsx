68311b3bbfb6cbea8c70d866ae68849c55b957f6
FelixMembrive <felix.membrive@gmail.com>
2022-07-23 00:16:00 -0300

    Footer Responsivo

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter } from 'react-router-dom';
-import Footer from './componentes/Footer';
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Footer />
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 )
