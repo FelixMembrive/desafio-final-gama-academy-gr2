403c8186f1775ec7ac41f9d461b5358d373f57c8
Beatriz Gebhardt <beatrizgra@outlook.com>
2022-07-23 21:41:17 -0300

    add features

---
+++ src/componentes/FeatureCard/index.tsx
@@ -0,0 +1,21 @@
+import VideoModal from "../VideoModal";
+import { Description, Iframe, Title, Placeholder, Wrapper } from "./styles";
+
+interface Props {
+  title: string;
+  description: string;
+  link: string;
+}
+
+const FeatureCard = (props: Props) => {
+  return (
+    <Wrapper>
+      <VideoModal src={props.link} />
+      <Placeholder />
+      <Title>{props.title}</Title>
+      <Description>{props.description}</Description>
+    </Wrapper>
+  );
+};
+
+export default FeatureCard;
---
+++ src/componentes/FeatureCard/styles.ts
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  max-width: 408px;
+  display: flex;
+  flex-direction: column;
+  position: relative;
+`;
+
+export const Iframe = styled.iframe``;
+export const Title = styled.h5`
+  margin-top: 30px;
+`;
+export const Description = styled.p`
+  text-align: start;
+`;
+export const Placeholder = styled.div`
+  display: flex;
+  height: 400px;
+  width: 408px;
+  background-color: #d9d9d9;
+  border-radius: 10px;
+`;
