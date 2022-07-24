9c66d3a27d2bb72ea4d2c175993396c481ed86f9
Amir Moufarrege <amir0883@yahoo.com.br>
2022-07-23 21:41:09 -0300

    finalizing hero

--- src/pages/PaginaAmir/index.tsx
+++ src/pages/PaginaAmir/index.tsx
@@ -1,41 +1,56 @@
 import { Container } from "react-bootstrap";
 // import Button from "react-bootstrap/Button";
-import ex1 from "../../assets/imagens/ex1.jpg";
+import img_400_390 from "../../assets/imagens/img_400_390.png";
+import Emplog from "../../assets/imagens/Emplog_360_150_46.png";
 import LinkButton from "../../componentes/LinkButton";
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 // import "./styles.css"
 
 export default function PaginaAmir() {
     return (
-        <Container className="ps-5">
-            <div className="d-flex">
-                <div>
-                    <div>
-                        <h2 className="tx1 fs-2 fw-bolder text-start mt-5 pt-5 mb-3">Frase de impacto sobre <br></br> o principal diferencial da empresa</h2>
-                        <p className="tx2 fw-semibold text-start mb-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Donec vel<br></br> risus a nibh placerat volutpat eget vitae leo.</p>
-                    </div>
-                    <div className="mt-4">
-                    <LinkButton className="btn-secondary" to="" text="Registre-se"></LinkButton>
-                    <LinkButton className="btn-secondary ms-3" to="" text="Conheça mais"></LinkButton>
-                    </div>
-                </div>
-                <div>
-                    <img className="mt-5 pt-4 ms-5 h-75 " src={ex1} height={390} width={400}/>
-                </div>
-            </div>
 
+                <Container fluid="sm">
 
-            <div>
-                <div>
-                    <p className="fw-bolder mt-4 me-5 pe-5 ps text-center ">Empresas parceiras</p>
-                </div>
-                <div>
-                    <img src="#2" />
-                    <img src="#3" />
-                    <img src="#4" />
-                    <img src="#5" />
-                </div>
-            </div>
-        </Container>
-    );
+                <Row className="d-flex flex-column-reverse flex-md-row">
+                    <Col sm={6} className=""> 
+                        <div >
+                            <h2 className="tx1 fs-2 lh-base  fw-bolder text-start mt-md-5 pt-md-5 mb-4">Frase de impacto sobre <br></br> o principal diferencial da empresa</h2>
+                            <p className="tx2 fw-normal text-start">Lorem ipsum dolor sit amet consectetur adipisicing elit. Donec vel<br></br> risus a nibh placerat volutpat eget vitae leo.</p>
+                        </div>
+
+                        <div className="d-flex flex-column flex-md-row mt-4">
+                            <LinkButton className="btn-secondary rounded-pill mt-2" to="" text="Registre-se"></LinkButton>
+                            <LinkButton className="btn-light border border-dark border-2 rounded-pill ms-0 ms-md-4 mt-2" to="" text="Conheça mais"></LinkButton>
+                        </div>
+                    </Col>
+
+                    <Col sm={6}>
+                            <img className=" mt-4 mb-md-5 pe-2 pt-4 h-75 " src={img_400_390} height={370} width={380}/>
+                    </Col>
+                </Row>
+
+                <Row>
+                    <Col>
+                        <p className="fw-bolder mt-4 me-5 pe-5 ps text-center ">Empresas parceiras</p>
+                    </Col>
+                </Row>
+
+                <Row>
+                    <Col className="text-center">
+                       <div className="">
+                            <img className="me-4 mt-4" src={Emplog} width={140}/>
+                            <img className="me-4 mt-4" src={Emplog} width={140}/>
+                            <img className="me-4 mt-4" src={Emplog} width={140}/>
+                            <img className="me-4 mt-4" src={Emplog} width={140}/>
+                            
+                           
+                        </div>
+                    </Col>
+                </Row>
+
+              </Container>
+
+         );
 }
