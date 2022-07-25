d51b48c9d4e15d9a55782ced1f0eed2ad636ebe8
Amir Moufarrege <amir0883@yahoo.com.br>
2022-07-24 21:58:41 -0300

    finalizando hero area candidata

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -3,6 +3,7 @@ import HomeDev from "./pages/HomeDev";
 import Home from "./pages/Home"
 import AreaCandidata from "./pages/AreaCandidata";
 import Pesquisa from "./pages/Pesquisa";
+import Amir_AreaCandidata from "./pages/Amir_AreaCandidata";
 
 
 
@@ -17,6 +18,7 @@ export default function RoutesComponent() {
         <Route path="" element={<Home />} />
         <Route path="/pesquisa" element={<Pesquisa />} />
         <Route path="/areacandidata" element={<AreaCandidata />} />
+        <Route path="/amir_areacandidata" element={<Amir_AreaCandidata />} />
       </Routes>
   );
 }
