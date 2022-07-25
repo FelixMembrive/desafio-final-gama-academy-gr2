de78129c28b411580e728d378ad97a21b32707a5
Beatriz Lisboa Sanchez <bia.lsanchez@gmail.com>
2022-07-25 19:03:31 -0300

    added vagas-recomendadas-candidata

--- src/pages/AreaCandidata/index.tsx
+++ src/pages/AreaCandidata/index.tsx
@@ -1,9 +1,11 @@
 import SecaoAssinarNewsletter from "../../componentes/SecaoAssinarNewsletter";
+import VagasRecomendadas from "../../componentes/VagasRecomendadas";
 
 export default function AreaCandidata() {
     return (
       <>
         <h1>PERFIL</h1>
+        <VagasRecomendadas />
         <SecaoAssinarNewsletter />
       </>
     );
