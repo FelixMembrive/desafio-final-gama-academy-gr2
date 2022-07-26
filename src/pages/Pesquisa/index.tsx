5a6ba036792280a8c11b47b98388901e01c5a5ad
Beatriz Lisboa Sanchez <bia.lsanchez@gmail.com>
2022-07-26 12:58:36 -0300

    added saving logic to cardVagas

--- src/pages/Pesquisa/index.tsx
+++ src/pages/Pesquisa/index.tsx
@@ -1,7 +1,14 @@
+import CardVaga from "../../componentes/CardVaga";
+
 export default function Pesquisa() {
     return (
       <>
         <h1>PESQUISA DE VAGAS</h1>
+        <CardVaga  nome_vaga="Nome da vaga" nome_empresa="Empresa" local_empresa="Local" salvo={true}/>
+        <br />
+        <CardVaga  nome_vaga="Nome da vaga" nome_empresa="Empresa" local_empresa="Local" salvo={false}/>
+        <br />
+        <CardVaga  nome_vaga="Nome da vaga" nome_empresa="Empresa" local_empresa="Local" salvo={null}/>
       </>
     );
   }
