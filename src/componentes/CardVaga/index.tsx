bd593e1c1d04be1573909b579853cbf37326c7f1
Beatriz Lisboa Sanchez <bia.lsanchez@gmail.com>
2022-07-26 13:03:33 -0300

    fixed minor bug

---
+++ src/componentes/CardVaga/index.tsx
@@ -0,0 +1,63 @@
+import CardVideo from "../CardVideo";
+import ScrollContainer from 'react-indiana-drag-scroll';
+import { Card, Row } from "react-bootstrap";
+import logo from '../../assets/icons/icone_vaga.svg';
+
+import './style.scss';
+import { ReactElement, useState } from "react";
+import { BookmarkOutline, Bookmark } from 'react-ionicons';
+
+interface ICardVagasProps {
+    nome_vaga: string,
+    nome_empresa: string,
+    local_empresa: string,
+    salvo: Boolean | null,
+    img?: string,
+}
+
+export default function CardVaga(props: ICardVagasProps) {
+    const [salvar, setSalvar] = useState(props.salvo === true);
+    const handleSalvar = () => {
+        setSalvar(true);
+    };
+    const handleNaoSalvar = () => {
+        setSalvar(false);
+    };
+    let icon: ReactElement | null = null;
+
+    if (props.salvo !== null) {
+        if (salvar) {
+            icon = <div className="icone-vagas-salvar m-0 p-0 d-flex">
+                <Bookmark
+                    onClick={handleNaoSalvar}
+                    color={'#00000'}
+                    height="100%"
+                    width="100%"
+                />;
+            </div>;
+        } else {
+            icon = <div className="icone-vagas-salvar m-0 p-0 d-flex">
+                <BookmarkOutline
+                    onClick={handleNaoSalvar}
+                    color={'#00000'}
+                    height="100%"
+                    width="100%"
+                />;
+            </div>;
+        }
+    }
+
+    return (
+        <Card className="card-vagas border-0 p-xs-4 p-lg-7">
+            <Card.Body className="card-vagas-body d-flex m-0 p-0">
+                <img src={props.img ? props.img : logo} alt={`logo da empresa ${props.nome_empresa}`} className="img-vagas-candidata me-8 me-10 img-fluid" />
+                <div className="card-vagas-text">
+                    <p className="m-0">{props.nome_vaga}</p>
+                    <p className="m-0">{props.nome_empresa}</p>
+                    <p className="m-0">{props.local_empresa}</p>
+                </div>
+                {icon}
+            </Card.Body>
+        </Card>
+    );
+}
--- src/pages/Pesquisa/index.tsx
+++ src/pages/Pesquisa/index.tsx
@@ -1,7 +1,12 @@
+import CardVaga from "../../componentes/CardVaga";
+
 export default function Pesquisa() {
     return (
       <>
         <h1>PESQUISA DE VAGAS</h1>
+        <CardVaga nome_empresa="Empresa" local_empresa="Local" nome_vaga="Nome da Vaga" salvo={true}/>
+        <CardVaga nome_empresa="Empresa" local_empresa="Local" nome_vaga="Nome da Vaga" salvo={false}/>
+        <CardVaga nome_empresa="Empresa" local_empresa="Local" nome_vaga="Nome da Vaga" salvo={null}/>
       </>
     );
   }
