f36ca1b8401cd7ef969ae2587963300e4b779f8d
FelixMembrive <felix.membrive@gmail.com>
2022-07-25 21:47:08 -0300

    Ajustes de Responsividade na Seção Assinar Newsletter

--- src/pages/AreaCandidata/index.tsx
+++ src/pages/AreaCandidata/index.tsx
@@ -1,12 +1,15 @@
+import Footer from "../../componentes/Footer";
+import Header from "../../componentes/Header";
 import SecaoAssinarNewsletter from "../../componentes/SecaoAssinarNewsletter";
 import VagasRecomendadas from "../../componentes/VagasRecomendadas";
 
 export default function AreaCandidata() {
     return (
       <>
-        <h1>PERFIL</h1>
+        <Header />
         <VagasRecomendadas />
         <SecaoAssinarNewsletter />
+        <Footer />
       </>
     );
   }
