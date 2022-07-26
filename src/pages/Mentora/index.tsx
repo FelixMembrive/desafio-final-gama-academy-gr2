6a21086955312826759d8b8c887c1cad93dc59af
“Pedro <pedro_brandao2@hotmail.com>
2022-07-26 17:35:38 -0300

    background bug fix

--- src/pages/Mentora/index.tsx
+++ src/pages/Mentora/index.tsx
@@ -7,11 +7,12 @@ export default function Mentora() {
   return (
     <>
       <Header />
-      <h5 className="px-xs-10 px-sm-60 px-md-20 px-lg-16 pt-5">Cursos relacionados</h5>
-      <p className="px-xs-10 px-sm-60 px-md-20 px-lg-16 pb-5">Veja cursos que a mentora indica</p>
 
+        <div>
+          <h5 className="px-xs-10 px-sm-60 px-md-20 px-lg-16 pt-5">Cursos relacionados</h5>
+          <p className="px-xs-10 px-sm-60 px-md-20 px-lg-16 pb-5">Veja cursos que a mentora indica</p>
         <CardCurso />
-
+        </div>
       <Footer />
     </>
   );
