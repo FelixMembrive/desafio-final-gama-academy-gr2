9ff7a9549ad57ea2b154f59a24e9e161ffb86891
Beatriz Lisboa Sanchez <bia.lsanchez@gmail.com>
2022-07-26 17:26:11 -0300

    edited cardVagas

--- src/pages/BuscarVagas/index.tsx
+++ src/pages/BuscarVagas/index.tsx
@@ -1,3 +1,4 @@
+import CardVaga from "../../componentes/CardVaga";
 import LinkText from "../../componentes/LinkText";
 
 export default function BuscarVagas() {
@@ -5,6 +6,9 @@ export default function BuscarVagas() {
       <>
         <h1>PESQUISA DE VAGAS</h1>
         <LinkText backArrow to="javascript:history.back()" text="Voltar"></LinkText>
+        <CardVaga nome_empresa="Empresa" local_empresa="Local" nome_vaga="Nome da Vaga" salvo={true}/>
+        <CardVaga nome_empresa="Empresa" local_empresa="Local" nome_vaga="Nome da Vaga" salvo={false}/>
+        <CardVaga nome_empresa="Empresa" local_empresa="Local" nome_vaga="Nome da Vaga" salvo={null}/>
       </>
     );
   }
