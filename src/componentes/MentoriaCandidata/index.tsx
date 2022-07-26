891da5a60a86ca082a73d42d256d456ca83d7913
Beatriz Lisboa Sanchez <bia.lsanchez@gmail.com>
2022-07-26 11:35:39 -0300

    added component card-mentora

--- src/componentes/MentoriaCandidata/index.tsx
+++ src/componentes/MentoriaCandidata/index.tsx
@@ -5,6 +5,7 @@ import logo from '../../assets/icons/icone_vaga.svg'
 
 import './style.scss'
 import LinkButton from "../LinkButton";
+import CardMentora from "../CardMentora";
 
 export default function MentoriaCandidata() {
     const dados = [
@@ -42,17 +43,7 @@ export default function MentoriaCandidata() {
         <Row className="flex-nowrap row gap-xs-3 gap-lg-8 ms-0">
         {dados.map((cardInfo, index) => {
             return (
-                <Card className="card-mentoria border-0 p-xs-4 p-lg-7">
-                    <Card.Body className="card-mentoria-body d-flex flex-column m-0 p-0">
-                    <img src={logo} alt={`foto da mentora ${cardInfo.nome_mentora}`} className="img-mentoria-candidata m-auto img-fluid mb-2 mb-lg-3"/>
-                    <div className="card-mentoria-text">
-                        <p className="fw-semibold m-0 mb-1">{cardInfo.nome_mentora}</p>
-                        <p className="fw-semibold m-0 mb-2">{cardInfo.area_mentora}</p>
-                        <p>{cardInfo.descricao_mentora}</p>
-                    </div>
-                    <LinkButton className='rounded-pill m-auto d-flex justify-content-center align-items-center' to='#' text='Ver perfil'/>
-                    </Card.Body>
-                </Card>
+                <CardMentora nome_mentora={cardInfo.nome_mentora} area_mentora={cardInfo.area_mentora} descricao_mentora={cardInfo.descricao_mentora} key={index}/>
             )
         })}
         </Row>
