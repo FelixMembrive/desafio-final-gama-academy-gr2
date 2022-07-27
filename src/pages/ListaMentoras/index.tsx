ca6d38003a154f120b3590593bc87edbfe05b558
Beatriz Lisboa Sanchez <bia.lsanchez@gmail.com>
2022-07-26 21:01:03 -0300

    minor tweaks in ListaMentoras

--- src/pages/ListaMentoras/index.tsx
+++ src/pages/ListaMentoras/index.tsx
@@ -40,15 +40,20 @@ export default function ListaMentoras() {
       descricao_mentora: "Jornalista por formação, Product Designer na..."
     },
 
-  ]
+  ];
   return (
     <>
       <Header />
       <Mentoras />
-      {dados.map((item, index)=>{
-        return(
-          <CardMentora key={index} nome_mentora={item.nome_mentora} area_mentora={item.area_mentora} descricao_mentora={item.descricao_mentora}/>
-        )
+      {dados.map((item, index) => {
+        return (
+          <CardMentora 
+          className="card-mentora-lista"
+          key={index} 
+          nome_mentora={item.nome_mentora} 
+          area_mentora={item.area_mentora} 
+          descricao_mentora={item.descricao_mentora} />
+        );
       })}
       <Footer />
     </>
