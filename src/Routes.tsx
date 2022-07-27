42c2887905e4eb41623a23cd69300fade2a82727
Marcelo Barbosa <celo.filho1@hotmail.com>
2022-07-27 17:28:47 -0300

    Login page verção mobile + componentes(labelinput e socialLinkLogIN)

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,29 +1,39 @@
-import { Routes, Route, Link } from "react-router-dom";
-import HomeDev from "./pages/HomeDev";
-import Home from "./pages/Home"
-import AreaCandidata from "./pages/AreaCandidata";
-import Amir_AreaCandidata from "./pages/Amir_AreaCandidata";
-import ListaMentoras from "./pages/ListaMentoras";
-import MinhasCandidaturasAndamento from "./pages/MinhasCandidaturasAndamento";
-import MinhasCandidaturasSalvas from "./pages/MinhasCandidaturasSalvas";
-import MinhasCandidaturasFinalizadas from "./pages/MinhasCandidaturasFinalizadas";
-import BuscarVagas from "./pages/BuscarVagas";
-import Mentora from "./pages/Mentora";
-
+import { Routes, Route, Link } from 'react-router-dom'
+import HomeDev from './pages/HomeDev'
+import Home from './pages/Home'
+import AreaCandidata from './pages/AreaCandidata'
+import Amir_AreaCandidata from './pages/Amir_AreaCandidata'
+import ListaMentoras from './pages/ListaMentoras'
+import MinhasCandidaturasAndamento from './pages/MinhasCandidaturasAndamento'
+import MinhasCandidaturasSalvas from './pages/MinhasCandidaturasSalvas'
+import MinhasCandidaturasFinalizadas from './pages/MinhasCandidaturasFinalizadas'
+import BuscarVagas from './pages/BuscarVagas'
+import Mentora from './pages/Mentora'
+import Login from './pages/Login'
 
 export default function RoutesComponent() {
   return (
-      <Routes>
-        <Route path="" element={<HomeDev />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/buscarvagas" element={<BuscarVagas />} />
-        <Route path="/areacandidata" element={<AreaCandidata />} />
-        <Route path="/amir_areacandidata" element={<Amir_AreaCandidata />} />
-        <Route path="/candidaturas-salvas" element={<MinhasCandidaturasSalvas/>} />
-        <Route path="/candidaturas-andamento" element={<MinhasCandidaturasAndamento/>} />
-        <Route path="/candidaturas-finalizadas" element={<MinhasCandidaturasFinalizadas/>} />
-        <Route path="/listamentoras" element={<ListaMentoras />} />
-        <Route path="/mentora" element={<Mentora />} />
-      </Routes>
-  );
+    <Routes>
+      <Route path="" element={<HomeDev />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/buscarvagas" element={<BuscarVagas />} />
+      <Route path="/areacandidata" element={<AreaCandidata />} />
+      <Route path="/amir_areacandidata" element={<Amir_AreaCandidata />} />
+      <Route
+        path="/candidaturas-salvas"
+        element={<MinhasCandidaturasSalvas />}
+      />
+      <Route
+        path="/candidaturas-andamento"
+        element={<MinhasCandidaturasAndamento />}
+      />
+      <Route
+        path="/candidaturas-finalizadas"
+        element={<MinhasCandidaturasFinalizadas />}
+      />
+      <Route path="/listamentoras" element={<ListaMentoras />} />
+      <Route path="/mentora" element={<Mentora />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  )
 }
