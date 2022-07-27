d9a4ce116747ffd940c41f8a10b10ffc0df9578b
Beatriz Lisboa Sanchez <bia.lsanchez@gmail.com>
2022-07-26 21:46:35 -0300

    figured out the acordions styles

--- src/componentes/AcordionVaga/index.tsx
+++ src/componentes/AcordionVaga/index.tsx
@@ -19,19 +19,19 @@ interface IAcordionVagasProps {
 export default function AcordionVaga(props: IAcordionVagasProps) {
 
     return (
-        <Accordion className={`card-vagas border-0 p-0 m-0 ${props.className}`}>
-            <Accordion.Item className='card-vagas border-0 p-0 m-0' eventKey="0">
-            <Accordion.Header className="card-vagas-body d-flex justify-content-between align-items-center m-0 p-0">
+        <Accordion className={`acordion-vagas border-0 p-0 m-0 ${props.className}`}>
+            <Accordion.Item className='item-vagas border-0 p-0 m-0' eventKey="0">
+            <Accordion.Header className="acordion-vagas-body d-flex justify-content-between align-items-center m-0 p-0">
                 <div className='d-flex justify-content-start'>
-                    <img src={props.img ? props.img : logo} alt={`logo da empresa ${props.nome_empresa}`} className="img-card-vagas me-8 me-10 img-fluid" />
-                    <div className="card-vagas-text">
+                    <img src={props.img ? props.img : logo} alt={`logo da empresa ${props.nome_empresa}`} className="img-acordion-vagas me-8 me-10 img-fluid" />
+                    <div className="acordion-vagas-text">
                         <p className="m-0">{props.nome_vaga}</p>
                         <p className="m-0">{props.nome_empresa}</p>
                         <p className="m-0">{props.local_empresa}</p>
                     </div>
                 </div>
             </Accordion.Header>
-            <AccordionBody>
+            <AccordionBody className="acordion-vagas-body">
                 <p>HI</p>
             </AccordionBody>
             </Accordion.Item>
