d0d8a3f25faae1d67b67bf0a65449b3eeed716a2
Beatriz Lisboa Sanchez <bia.lsanchez@gmail.com>
2022-07-28 15:46:32 -0300

    started adding mockup

--- src/componentes/CardVaga/index.tsx
+++ src/componentes/CardVaga/index.tsx
@@ -5,12 +5,12 @@ import logo from '../../assets/icons/icone_vaga.svg';
 
 import './style.scss';
 import { ReactElement, useState } from "react";
-// import { BookmarkOutline, Bookmark } from 'react-ionicons';
+import { BookmarkSimple } from 'phosphor-react'
 
 interface ICardVagasProps {
     nome_vaga: string,
     nome_empresa: string,
-    local_empresa: string,
+    local_empresa?: string,
     salvo: Boolean | null,
     img?: string,
     className?: string,
@@ -26,37 +26,40 @@ export default function CardVaga(props: ICardVagasProps) {
     };
     let icon: ReactElement | null = null;
 
-    // if (props.salvo !== null) {
-    //     if (salvar) {
-    //         icon = <div className="icone-vagas-salvar m-0 p-0 d-flex">
-    //             <Bookmark
-    //                 onClick={handleNaoSalvar}
-    //                 color={'#00000'}
-    //                 height="100%"
-    //                 width="100%"
-    //             />
-    //         </div>;
-    //     } else {
-    //         icon = <div className="icone-vagas-salvar m-0 p-0 d-flex">
-    //             <BookmarkOutline
-    //                 onClick={handleNaoSalvar}
-    //                 color={'#00000'}
-    //                 height="100%"
-    //                 width="100%"
-    //             />
-    //         </div>;
-    //     }
-    // }
+    if (props.salvo !== null) {
+        if (salvar) {
+            icon = <div className="icone-vagas-salvar m-0 p-0 d-flex">
+                <BookmarkSimple
+                    onClick={handleNaoSalvar}
+                    color="#000000"
+                    height="100%"
+                    width="100%"
+                    alt="salvar"
+                    weight="regular"
+                />
+            </div>;
+        } else {
+            icon = <div className="icone-vagas-salvar m-0 p-0 d-flex">
+                <BookmarkSimple
+                    onClick={handleSalvar}
+                    color="#000000"
+                    height="100%"
+                    width="100%"
+                    weight="fill"
+                    alt="salva"
+                />
+            </div>;
+        }
+    }
 
     return (
-        <Card className={`card-vagas border-0 p-xs-5 p-lg-7 ${props.className}`}>
+        <Card className={`card-vagas border-0 p-3 p-lg-4 ${props.className}`}>
             <Card.Body className="card-vagas-body d-flex justify-content-between align-items-center m-0 p-0">
                 <div className='d-flex justify-content-start '>
-                    <img src={props.img ? props.img : logo} alt={`logo da empresa ${props.nome_empresa}`} className="img-card-vagas me-8 me-10 img-fluid" />
+                    <img src={props.img ? props.img : logo} alt={`logo da empresa ${props.nome_empresa}`} className="img-card-vagas me-3 melg--5 img-fluid" />
                     <div className="card-vagas-text">
                         <p className="m-0">{props.nome_vaga}</p>
                         <p className="m-0">{props.nome_empresa}</p>
-                        <p className="m-0">{props.local_empresa}</p>
                     </div>
                 </div>
                 {icon}
