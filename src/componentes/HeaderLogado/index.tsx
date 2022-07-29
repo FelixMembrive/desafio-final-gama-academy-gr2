5ae9669ca644f1ff73ee19f53bb297cfd8066ecb
Mayume Cristine <mayumecristine@icloud.com>
2022-07-29 19:42:39 -0300

    Header com alta fidelidade

--- src/componentes/HeaderLogado/index.tsx
+++ src/componentes/HeaderLogado/index.tsx
@@ -1,32 +1,31 @@
-import Logo from "../../assets/icons/Logo.png";
-import Perfil from "../../assets/imagens/foto_perfil.png"
+import Logo from "../../assets/icons/logo-techdelas.png";
 import "./style.scss";
 
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
+import Button from "react-bootstrap/Button";
 
-export default function HeaderLogado() {
+export default function Header() {
   return (
-    <Navbar id="navStyle" expand="lg">
-      <Container id="containerStyle">
-      <Navbar.Brand id="navImg" href="#home">
+    <Navbar id="navStyle-logado" expand="lg">
+      <Container id="containerStyle-logado">
+        <Navbar.Brand id="navImg-logado" href="#home">
           <img src={Logo} 
           className="d-inline-block align-top" 
           alt="Logo" 
           id="logo"
           />
         </Navbar.Brand>
-        <Navbar.Toggle  id="botaoToggle"/>
-        <img src={Perfil}  alt="PFC" id="imagem"/>
-        <Navbar.Collapse id="basic-navbar-nav">
-          <div className="bd-toc-item">
+        <Navbar.Toggle  id="botaoToggleHeaderLogado"/>
+        <Navbar.Collapse id="basic-navbar-nav-logado">
+          <div className="bd-toc-item-logado">
             <Nav.Link href="/buscarvagas" className="links">Área da candidata</Nav.Link>
-            <Nav.Link className="links">Vagas</Nav.Link>
+            <Nav.Link href="#link" className="links">Vagas</Nav.Link>
             <Nav.Link href="#link" className="links">Cursos</Nav.Link>
-            <Nav.Link href="#link" className="links">Mentorias</Nav.Link>
+            <Nav.Link href="#link" className="links">Mentoras</Nav.Link>
             <Nav.Link href="#link" className="links">Blog</Nav.Link>
-            <Nav.Link href="#link" className="links"  style={{color:"red"}}>Sair da conta</Nav.Link>
+            <Nav.Link href="#link" className="links">Notificações</Nav.Link>
           </div>
         </Navbar.Collapse>
       </Container>
