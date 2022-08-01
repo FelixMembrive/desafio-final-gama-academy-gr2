7cb985f19fb9f9fa6c4f3249a12f3b4340805afd
Mayume Cristine <mayumecristine@icloud.com>
2022-08-01 16:25:47 -0300

    Iniciando alta fidelidade página Mentoras

--- src/componentes/Mentoras/index.tsx
+++ src/componentes/Mentoras/index.tsx
@@ -9,7 +9,7 @@ export default function Mentoras() {
         <div className="containerMentorias" style={{backgroundColor:'white'}}>
             <div className='Mentorias'>
                 <h2>Mentoras</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel risus a nibh placerat volutpat eget vitae leo. Curabitur sapien purus, laoreet vel sollicitudin id, rutrum a urna. Sed mattis commodo libero, sit amet tempus eros molestie eget. Praesent elementum nisi id massa mattis, at interdum neque vulputate. In vulputate velit consectetur tortor tempus mollis.</p>
+                <p>As mentoras da Tech Delas são profissionais extremamente capacitadas e que amam compartilhar seus conhecimentos e ajudar outras mulheres se desenvolverem nas suas carreiras.</p>
             </div>
             <div className="botao">
                 <DropdownButton id="dropdown" title="Escolha a área de atuação profissional" >
