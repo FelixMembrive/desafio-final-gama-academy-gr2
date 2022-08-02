aa1ef8c14eb144b5dc6e8ddc786d51b891235b9d
Beatriz Lisboa Sanchez <bia.lsanchez@gmail.com>
2022-08-02 08:33:16 -0300

    solved css bug

--- src/componentes/ProcessosSeletivos/index.tsx
+++ src/componentes/ProcessosSeletivos/index.tsx
@@ -6,8 +6,7 @@ import ImagemTriangulos from '../../assets/imagens/placeholder_triangulos.svg';
 export default function ProcessosSeletivos() {
 
     return (
-        <div className="process-section d-flex flex-column flex-lg-row-reverse px-8 py-9 px-sm-19 py-sm-19 py-lg-30 justify-content-xl-between align-items-lg-end pb-lg-50 pb-xxl-76" style={{backgroundColor:'white'}}>
-                <img src={ImagemTriangulos} alt="" className="img-processos img-fluid bg-transparent mx-3 mb-8 mx-md-5 mb-lg-0"/>
+        <div className="process-section">
                 <div className='process-text bg-transparent d-flex flex-column'>
                     <h2 className=" h2-processos bg-transparent align-self-center align-self-lg-start">Processos Seletivos</h2>
                     <p className="texto-processos bg-transparent mb-8 align-self-center align-self-lg-start text-center text-lg-start">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel risus a nibh placerat volutpat eget vitae leo.</p>
