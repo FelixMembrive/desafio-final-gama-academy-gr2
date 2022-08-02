054f7887c98c0641d857e634761714f875ca6574
Beatriz Lisboa Sanchez <bia.lsanchez@gmail.com>
2022-08-01 21:00:46 -0300

    added card-video alta-fidelidade

--- src/componentes/CardVideo/index.tsx
+++ src/componentes/CardVideo/index.tsx
@@ -2,6 +2,8 @@ import { Col, Row } from "react-bootstrap";
 import Card from "react-bootstrap/Card";
 import './style.scss';
 import VideoModal from "../VideoModal";
+import image from "../../assets/imagens/imagem-video1.png";
+import '../../typography.scss';
 
 interface ICardVideoProps {
   videoTitle?: string;
@@ -9,19 +11,23 @@ interface ICardVideoProps {
   cardTitle: string;
   cardSubtitle: string;
   cardText: string;
+  imgSrc?: string;
 }
 
 export default function CardVideo(props: ICardVideoProps) {
   return (
     <Col className="card-col">
-      <div className="img rounded d-flex flex-column justify-content-end">
-        <VideoModal src={props.src} />
-        <div className="depo-info rounded mt-16 mt-lg-76">
-          <h3 className="fs-5 bg-transparent fw-semibold text-start my-1 ms-3">{props.cardTitle}</h3>
-          <p className="fs-8 subtitle text-start bg-transparent m-0 ms-3">{props.cardSubtitle}</p>
-          <p className="fs-6 text-start bg-transparent m-0 ms-3 mx-lg-4 mb-3 mb-lg-5 mt-2 mt-lg-3 lh-lg">{props.cardText}</p>
-        </div>
-      </div>
+      <Card className="img rounded d-flex flex-column justify-content-end">
+        <Card.Img src={image} className="m-0"/>
+        <VideoModal src={props.imgSrc? props.imgSrc : props.src} />
+        <Card.Body className="p-0">
+          <div className="depo-info rounded">
+            <h3 className="text-start tp-heading-4">{props.cardTitle}</h3>
+            <p className="text-start tp-body-small depo-info-cargo">{props.cardSubtitle}</p>
+            <p className="text-start tp-body-small">{props.cardText}</p>
+          </div>
+        </Card.Body>
+      </Card>
     </Col>
   );
 }
--- src/componentes/VagaFinalizada/index.tsx
+++ src/componentes/VagaFinalizada/index.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import AcordionVaga from "../AcordionVaga";
 import './style.scss';
 import { Link } from "react-router-dom";
