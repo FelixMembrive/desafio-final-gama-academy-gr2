4a6afb1eea163c7ede47e955cabea78ce85a827b
“Pedro <pedro_brandao2@hotmail.com>
2022-08-02 16:09:29 -0300

    improved the layout on mentora page

--- src/pages/Mentora/index.tsx
+++ src/pages/Mentora/index.tsx
@@ -11,15 +11,17 @@ export default function Mentora() {
   return (
     <>
       <Header />
-      <LinkBack text="Voltar para Mentoras"/>
-      <DescricaoMentora /> 
-      <div >
+      <LinkBack text="Voltar para Mentoras" className="pt-5 ps-md-3"/>
+      <div className="ps-md-3 ps-lg-5">
+      <DescricaoMentora/> 
+      <div>
       <LinkButton className="d-flex justify-content-center rounded-pill py-2 ms-4 my-5 btn-valores-agendamento" to="" text="Ver valores para agendamento"></LinkButton>
       </div> 
       <div className="conteudos-recomendados">
         <h5>Conteúdos recomendados</h5>
         <p>Veja materiais que a mentora indica</p>
         <CardCurso />
+      </div>
       </div>
       <Footer />
     </>
