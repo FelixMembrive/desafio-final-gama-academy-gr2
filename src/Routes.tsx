4b50eb3fd06a521722bdff61da0898adeef4138a
Amir Moufarrege <amir0883@yahoo.com.br>
2022-08-03 11:25:46 -0300

    criando pasta

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -4,6 +4,7 @@ import Home from "./pages/Home";
 import AreaCandidata from "./pages/AreaCandidata";
 import Amir_AreaCandidata from "./pages/Amir_AreaCandidata";
 import Amir_ValorMentoria from "./pages/Amir_ValorMentoria";
+import Amir_Redirec_ValorMentoria from "./pages/Amir_Redirec_ValorMentoria";
 import ListaMentoras from "./pages/ListaMentoras";
 import MinhasCandidaturas from "./pages/MinhasCandidaturas";
 import BuscarVagas from "./pages/BuscarVagas";
@@ -19,6 +20,7 @@ export default function RoutesComponent() {
       <Route path="/areacandidata" element={<AreaCandidata />} />
       <Route path="/amir_areacandidata" element={<Amir_AreaCandidata />} />
       <Route path="/amir_valormentoria" element={<Amir_ValorMentoria />} />
+      <Route path="/amir_redirecvalormentoria" element={<Amir_Redirec_ValorMentoria />} />
       <Route path="/candidaturas" element={<MinhasCandidaturas />} />
       <Route path="/listamentoras" element={<ListaMentoras />} />
       <Route path="/mentora" element={<Mentora />} />
