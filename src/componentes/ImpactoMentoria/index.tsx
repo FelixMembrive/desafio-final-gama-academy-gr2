545e638d07848964a7d9ba2a682ec60c70e3450c
Mayume Cristine <mayumecristine@icloud.com>
2022-08-03 00:53:40 -0300

    Impacto mentoria alta fidelidade mobile

--- src/componentes/ImpactoMentoria/index.tsx
+++ src/componentes/ImpactoMentoria/index.tsx
@@ -11,7 +11,7 @@ export default function ImpactoMentoria() {
             <h2>Seja mentorada pelas melhores do mercado</h2>
             <div className='Mentoria'>
                     <div id="backgroundMentoria" style={{backgroundColor:'#000000'}}></div>
-                    <p >Tire todas as suas dúvidas e tenha uma especialista para te ajudar a se destacar na sua carreira.</p>
+                    <p>Tire todas as suas dúvidas e tenha uma especialista para te ajudar a se destacar na sua carreira.</p>
                     <button className="botaoImpactoMentoria">Conheça as mentorias</button>
             </div>
         </div>
