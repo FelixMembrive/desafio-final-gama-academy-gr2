95ba57779ae00fe2be252a0c207db5ff79f13f45
FelixMembrive <felix.membrive@gmail.com>
2022-08-02 23:09:57 -0300

    Seção Assinar da pagina da candidata, sem responsividade

--- src/componentes/SecaoAssinarNewsletter/index.tsx
+++ src/componentes/SecaoAssinarNewsletter/index.tsx
@@ -10,14 +10,12 @@ export default function SecaoAssinarNewsletter() {
 
     return (
         <section className="assinar-newsletter">
-
-            <Title>Assine nossa newsletter</Title>
-
+            <h3 className="title-assine">Assine nossa newsletter</h3>
             <div className="conteudo-assinar-newsletter">
                 <input placeholder="Digite seu e-mail" type="email" className="item-assinar-newsletter input-assinar-newsletter" />
-                <SelectBox
+                {/* <SelectBox
                     className="item-assinar-newsletter select-assinar-newsletter"
-                    placeholder="Sua área"
+                    placeholder="Sua área profissional"
                     // inputId="sua-area-select"
                     optionValues={[1, 2, 3, 4, 5, 6, 7]}
                     optionTexts={[
@@ -29,9 +27,9 @@ export default function SecaoAssinarNewsletter() {
                         "Odontologia",
                         "Psicologia",
                     ]}
-                />
+                /> */}
                 <SelectReact />
-                <LinkButton to={''} text={'Inscreva-se'} className="item-assinar-newsletter button-assinar-newsletter" />
+                <LinkButton to={""} text={'Cadastre-se'} className="item-assinar-newsletter button-assinar-newsletter" />
 
             </div>
                 
