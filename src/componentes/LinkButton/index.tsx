c083920b3fb6740433bbe27ebb3499b098825923
Beatriz Lisboa Sanchez <bia.lsanchez@gmail.com>
2022-08-03 21:44:19 -0300

    adicionados espaçamento e fotos depoimentos

--- src/componentes/LinkButton/index.tsx
+++ src/componentes/LinkButton/index.tsx
@@ -1,5 +1,6 @@
 import { LinkContainer } from "react-router-bootstrap";
 import Button from "react-bootstrap/Button";
+import './style.scss'
 
 interface ILinkButtonProps {
   to: string;
@@ -13,8 +14,8 @@ interface ILinkButtonProps {
 
 export default function LinkButton(props: ILinkButtonProps) {
   return (
-    <LinkContainer to={props.to}>
-      <Button variant={props.variant} className={props.className} id={props.id}>
+    <LinkContainer to={props.to} className="link-button-no-shadow">
+      <Button variant={props.variant} className={`link-button-no-shadow ${props.className}`} id={props.id}>
         {props.iconRight}
         {props.text}
         {props.iconLeft}
