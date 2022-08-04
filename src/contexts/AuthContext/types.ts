242d9aa979fd4a805de8942f66a4fc578fdf39e8
FelixMembrive <felix.membrive@gmail.com>
2022-08-04 09:18:44 -0300

    Inicio da integração de cadastro

---
+++ src/contexts/AuthContext/types.ts
@@ -0,0 +1,26 @@
+export interface IUser {
+    email?: string;
+    token?: string;
+  }
+  
+  export interface IContext extends IUser {
+    authenticated: boolean;
+    user: IUser | null | undefined;
+    login: (
+      name: string,
+      email: string,
+      password: string,
+      phone: string,
+      profilePicture: string
+      ) => void;
+    logout: () => void;
+    // loading: boolean;
+    // writeError: (email: string, password: string) => void;
+    // errorResponse: any;
+    // setErrorResponse: any;
+  }
+  
+  export interface IAuthProvider {
+    children: JSX.Element;
+  }
+  
--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,17 +1,56 @@
+import { useContext, useEffect, useState } from "react";
 import Header from '../../componentes/Header'
 import Footer from '../../componentes/Footer'
 import SocialLinkLogIn from '../../componentes/SocialLinkLogIn'
-import { Link } from 'react-router-dom'
 import { LabelInput } from '../../componentes/LabelInput'
 import LinkButton from '../../componentes/LinkButton'
 import Divider from '../../componentes/Divider'
 import rightPattern from '../../assets/icons/rightPattern.svg'
+import { AuthContext, AuthProvider } from "../../contexts/AuthContext";
 import './style.scss'
+import { requestApiMultiPart } from "../../services/api";
+
 export default function RegisterPage() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [phone, setPhone] = useState("");
+  const [profilePicture, setProfilePicture] = useState({[]});
+  // const [phone, setPhone] = useState("");
+  // const [profilePicture, setProfilePicture] = useState("");
+  // const { login } = useContext(AuthContext);
+
+  async function login(name: string, email: string, password: string, phone: string, profilePicture: string) {
+    try {
+      const response = await requestApiMultiPart.post("/users", {
+        name: name,
+        email: email,
+        password: password,
+        phone: phone,
+        profilePicture: profilePicture
+      });
+      //   localStorage.setItem("user", JSON.stringify(response.data));
+      //   setUser(response.data);
+      //   navigate("/home");
+      //   return await response.data;
+    } catch (error) {
+      "error";
+    }
+  }
+
+  function handleSubmit(e: any) {
+    e.preventDefault();
+    if (password == confirmPassword) {
+      login(name, email, password, phone, profilePicture);
+    }
+  }
+
   return (
-    <>
+    <AuthProvider>
+      <>
       <Header />
-      <section className="registerPage">
+      <form onSubmit={handleSubmit} className="registerPage">
         <h2>Olá! Como você prefere se cadastrar?</h2>
         <div className="socialLinkLogInButtons">
           <SocialLinkLogIn
@@ -32,20 +71,64 @@ export default function RegisterPage() {
         </div>
         <Divider />
         <div className="registerInputs">
-          <LabelInput title="nome" inputType="text" placeholder="Seu nome" />
-          <LabelInput title="email" inputType="email" placeholder="Seu email" />
           <LabelInput
-            title="senha"
+            key={"name"}
+            title="name"
+            inputType="text"
+            placeholder="Seu nome"
+            value={name}
+            onChange={(e: any) => setName(e.target.value)}
+          />
+          {/* <p className={`absolute -mt-6 mx-2 text-[12px] ${errorResponse ? "text-alert" : ""}`}></p> */}
+
+          <LabelInput
+            key="email"
+            title="email"
+            inputType="email"
+            placeholder="Seu email"
+            value={email}
+            onChange={(e: any) => setEmail(e.target.value)}
+          />
+
+          <LabelInput
+            key={"phone"}
+            title="phone"
+            inputType="text"
+            placeholder="Digite seu Telefone"
+            value={phone}
+            onChange={(e: any) => setPhone(e.target.value)}
+          />
+
+          <LabelInput
+            key={"password"}
+            title="password"
             inputType="password"
             placeholder="Digite sua nova senha"
+            value={password}
+            onChange={(e: any) => setPassword(e.target.value)}
           />
+
           <LabelInput
-            title="confirmarSenha"
+            key={"confirmPassword"}
+            title="confirmPassword"
             inputType="password"
             placeholder="Re-digite sua senha"
+            value={confirmPassword}
+            onChange={(e: any) => setConfirmPassword(e.target.value)}
           />
+
+          <LabelInput
+            key={"profilePicture"}
+            title="profilePicture"
+            inputType="file"
+            placeholder="Carregar..."
+            value={profilePicture}
+            onChange={(e: any) => setProfilePicture(e.target.files[0])}
+          />
+
         </div>
-        <LinkButton className="registerButton" text="Cadastrar" to="#" />
+        {/* <LinkButton className="registerButton" text="Cadastrar" to="#" /> */}
+        <button className="registerButton" type="submit">ENTRAR</button>
         <p className="conditionsAndTerms">
           Ao continuar, você reconhece que leu e concordou com os{' '}
           <span>Termos de Serviço</span> e as <span>Regras de Privacidade</span>{' '}
@@ -53,8 +136,11 @@ export default function RegisterPage() {
         </p>
         <img className="rightPattern" src={rightPattern} alt="" />
         <img className="leftPattern" src={rightPattern} alt="" />
-      </section>
+
+      </form>
+
       <Footer />
-    </>
+      </>
+    </AuthProvider>
   )
 }
