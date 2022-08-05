8f934b60df9e7abc2436bfd3c93429f7a17c944b
FelixMembrive <felix.membrive@gmail.com>
2022-08-05 00:35:52 -0300

    Corrigindo bug de recarregar página

--- src/componentes/Header/index.tsx
+++ src/componentes/Header/index.tsx
@@ -22,9 +22,7 @@ export default function Header() {
         <Navbar.Collapse id="basic-navbar-nav">
           <div className="botaoStyle">
             <LinkButton id="cadastre" className="botoesHeader" to="/register" size="lg" text="Cadastre-se" />
-            <Button id="login" className="botoesHeader" href="/login" variant="outline" size="lg">
-              Entrar
-            </Button>
+            <LinkButton id="login" className="botoesHeader" to="/login" variant="outline" size="lg" text="Entrar"/>
           </div>
           <div className="bd-toc-item">
             <Nav.Link href="/buscarvagas" className="links">Vagas</Nav.Link>
