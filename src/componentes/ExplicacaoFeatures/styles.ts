929643273f6f600321a9b763d78af4ba3803e672
FelixMembrive <felix.membrive@gmail.com>
2022-08-05 02:01:18 -0300

    Ajustes na seção explicação features

--- src/componentes/ExplicacaoFeatures/styles.ts
+++ src/componentes/ExplicacaoFeatures/styles.ts
@@ -3,24 +3,10 @@ import background from "../../assets/imagens/servicosTechDelas/background.png";
 
 export const Wrapper = styled.div`
   display: flex;
-  width: 320px;
+  width: 100%;
   flex-direction: column;
   background-image: url(${background});
   background-repeat: no-repeat;
-  @media only screen and (max-width: 375px) {
-    width: 375px;}
-    @media only screen and (max-width: 390px) {
-    width: 390px;}
-    @media only screen and (max-width: 425px) {
-    width: 425px;}
-    @media only screen and (max-width: 768px) {
-    width: 768px;}
-    @media only screen and (max-width: 1024px) {
-    width: 768px;}
-    @media only screen and (max-width: 1440px) {
-    width: 768px;}
-    @media only screen and (max-width: 2560px) {
-    width: 2560px;}
 `;
 
 export const Title = styled.h2`
